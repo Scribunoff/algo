2e883f53ba91c7daabaa60636059c872dbae8fa2
DrDesperado <prokopenko.nv@phystech.edu>
2023-02-08 18:23:35 +0300

    refactored and tested InsertSort

--- src/main/java/Algo/InsertSort.java
+++ src/main/java/Algo/InsertSort.java
@@ -1,16 +1,24 @@
 package Algo;
 
-public class InsertSort {
-	public static void insertsort(int[] array) {
-		for (int i = 1; i < array.length; i++) {
-			int point = array[i];
+import java.util.List;
+
+public class InsertSort<T extends Comparable<T>> implements Sorting<T> {
+
+	@Override
+	public void sort(List<T> elements) {
+		for (int i = 1 ; i < elements.size() ; i++) {
+			T point = elements.get(i);
 			int j = i;
-			
-			while (j > 0 && array[j - 1] > point) {
-				array[j] = array[j-1];
-				j = j - 1;
+
+			while (j > 0 && large(elements.get(j - 1), point)) {
+				elements.set(j, elements.get(j - 1));
+				j--;
 			}
-			array[j] = point;
+			elements.set(j, point);
 		}
+	}
+
+	private boolean large(T k1, T k2) {
+		return k1.compareTo(k2) > 0;
 	}
 }
--- src/test/java/Algo/SortTest.java
+++ src/test/java/Algo/SortTest.java
@@ -8,13 +8,21 @@ import java.util.stream.Stream;
 import static org.junit.Assert.*;
 
 public class SortTest {
-    BubbleSort<Integer> bubbleSort = new BubbleSort<>();
     Integer[] elements = new Integer[]{5,8,2,5,7,8,1,3,79,2};
+    List<Integer> expected = Stream.of(elements).sorted().toList();
     @Test
     public void bubbleSortTesting() {
-        List<Integer> expected = Stream.of(elements).sorted().toList();
+        BubbleSort<Integer> bubbleSort = new BubbleSort<>();
         List<Integer> actual = new ArrayList<>(Arrays.stream(elements).toList());
         bubbleSort.sort(actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void insertSortTesting() {
+        InsertSort<Integer> integerInsertSort = new InsertSort<>();
+        List<Integer> actual = new ArrayList<>(Arrays.stream(elements).toList());
+        integerInsertSort.sort(actual);
         assertEquals(expected, actual);
     }
 }
